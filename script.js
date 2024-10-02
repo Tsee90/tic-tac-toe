4282fa894e97f2926230b04a769fb1b5a4ba4b93
Tsee90 <179171024+Tsee90@users.noreply.github.com>
2024-10-02 12:38:02 -0400

    Restructure game logic

--- script.js
+++ script.js
@@ -1,74 +1,78 @@
 const gameboard = (function(){
-    let a = [null, null, null];
-    let b = [null, null, null];
-    let c = [null, null, null];
+    let gameArray = [
+        '', '', '',
+        '', '', '',
+        '', '', ''
+    ];
+    const place = (index, symbol) => {
+        gameArray[index] = symbol;
+    }
+    const get = () => gameArray;
+    return {place, get}
+})();
 
-    const place = (x, y, player) => {
-        if(isOpen(x, y)){
-            let piece = '';
-            if(player === '1'){
-                piece = 'X';
-            }else{
-                piece = 'O';
-            }
-            switch (x){
-                case 'a':
-                    a[y] = piece;
-                    break;
-                case 'b':
-                    b[y] = piece;
-                    break;
-                case 'c':
-                    c[y] = piece;
-                    break;
-            }
-        }
-        if(isWinner()){
-            console.log('Player ' + player + ' wins!');
-        }
+function createPlayer(name, number) {
+    const changeName = (newName) => {name = newName}
+    const getName = () => name;
+    const getNumber = () => number;
+    return {changeName, getName, getNumber}
+}
+
+function game(index, player){
+    let piece = '';
+    if (player.getNumber() === '1') {
+        piece = 'X';
+    }else{
+        piece = 'O';
     }
 
-    const isOpen = (x, y) => {
-        switch (x){
-            case 'a':
-                if(a[y] === null){
-                    return true;
-                }else{
-                    return false;
-                }
-            case 'b':
-                if(b[y] === null){
-                    return true;
-                }else{
-                    return false;
-                }
-            case 'c':
-                if(c[y] === null){
-                    return true;
-                }else{
-                    return false
-                }
-        }
-    }
-
-    const isWinner = () => {
-        if(
-            a[0] === a[1] && a[0] === a[2] && a[0] !== null ||
-            b[0] === b[1] && b[0] === b[2] && b[0] !== null ||
-            c[0] === c[1] && c[0] === c[2] && c[0] !== null ||
-            a[0] === b[0] && a[0] === c[0] && a[0] !== null ||
-            a[1] === b[1] && a[1] === c[1] && a[1] !== null ||
-            a[2] === b[2] && a[2] === c[2] && a[2] !== null ||
-            a[0] === b[1] && a[0] === c[2] && a[0] !== null ||
-            a[2] === b[1] && a[2] === c[0] && a[2] !== null 
-        ){
+    const checkOpen = (index) => {
+        const arr = gameboard.get();
+        if (arr[index] === ''){
             return true;
         }else{
             return false;
         }
     }
 
-    const log = () => console.log(a + b + c);
+    const checkWin = () => {
+        const arr = gameboard.get();
+        if (
+            arr[0] === arr[1] && arr[0] === arr[2] && arr[0] !== '' ||
+            arr[3] === arr[4] && arr[3] === arr[5] && arr[3] !== '' ||
+            arr[6] === arr[7] && arr[6] === arr[8] && arr[6] !== '' ||
+            arr[0] === arr[3] && arr[0] === arr[6] && arr[0] !== '' ||
+            arr[1] === arr[4] && arr[1] === arr[7] && arr[1] !== '' ||
+            arr[2] === arr[5] && arr[2] === arr[8] && arr[2] !== '' ||
+            arr[0] === arr[4] && arr[0] === arr[8] && arr[0] !== '' ||
+            arr[2] === arr[4] && arr[2] === arr[6] && arr[2] !== ''
+        ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    const checkTie = () => {
+        const arr = gameboard.get();
+        let tie = true;
+        arr.forEach((item) => {
+            if (item === ''){
+                tie = false;
+            }
+        });
+        return tie;
+    }
 
-    return {place, log}
-})();
+    if (checkOpen(index)){
+        gameboard.place(index, piece);
+        if (checkWin()){
+            console.log(player.getName() + ' Wins!');
+        }else if(checkTie()){
+            console.log('TIE!');
+        }
+    }
+
+}
+
+const player1 = createPlayer('Player 1', '1');
+const player2 = createPlayer('Player 2', '2');
